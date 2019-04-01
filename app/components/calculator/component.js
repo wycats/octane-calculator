d4128abb9422c499d7df1edfd68dd18b32236c54
Yehuda Katz <wycats@gmail.com>
2019-04-01 08:38:08 -0700

    Separate decimal and whole

--- app/components/calculator/component.js
+++ app/components/calculator/component.js
@@ -4,24 +4,30 @@ import { tracked } from "@glimmer/tracking";
 
 export default class Calculator extends Component {
   @tracked current = new CalculatorNumber(0, 0);
+  @tracked operation = new WholeOperation();
 
   get rendered() {
-    return this.current.rendered();
+    return this.current.rendered(this.operation instanceof DecimalOperation ? "force-decimal" : false);
   }
 
   @action
   append(e) {
-    this.current = this.current.appendToWhole(parseInt(e.target.value));
+    [this.current, this.operation] = this.operation.append(this.current, parseInt(e.target.value));
   }
 
   @action
   zero() {
-    this.current = this.current.appendZero();
+    [this.current, this.operation] = this.operation.zero(this.current);
   }
 
   @action
   backspace() {
-    this.current = this.current.deleteFromWhole();
+    [this.current, this.operation] = this.operation.backspace(this.current);
+  }
+
+  @action
+  point() {
+    this.operation = new DecimalOperation();
   }
 }
 
@@ -31,9 +37,13 @@ class CalculatorNumber {
     this.decimal = decimal;
   }
 
-  rendered() {
+  rendered(force) {
     if (this.decimal === 0) {
-      return `${this.whole}`;
+      if (force === 'force-decimal') {
+        return `${this.whole}.`;
+      } else {
+        return `${this.whole}`;
+      }
     } else {
       return `${this.whole}.${this.decimal}`;
     }
@@ -49,5 +59,51 @@ class CalculatorNumber {
 
   deleteFromWhole() {
     return new CalculatorNumber(Math.floor(this.whole / 10), this.decimal);
+  }
+
+  appendToDecimal(number) {
+    return new CalculatorNumber(this.whole, this.decimal * 10 + number);
+  }
+
+  deleteFromDecimal() {
+    return new CalculatorNumber(this.whole, Math.floor(this.decimal / 10));
+  }
+}
+
+class WholeOperation {
+  @action
+  append(current, value) {
+    return [current.appendToWhole(value), this];
+  }
+
+  @action
+  zero(current) {
+    return [current.appendZero(), this];
+  }
+
+  @action
+  backspace(current) {
+    return [current.deleteFromWhole(), this];
+  }
+}
+
+class DecimalOperation {
+  @action
+  append(current, value) {
+    return [current.appendToDecimal(value), this];
+  }
+
+  @action
+  zero(current) {
+    return [current, this];
+  }
+
+  @action
+  backspace(current) {
+    if (current.decimal === 0) {
+      return [current, new WholeOperation()];
+    } else {
+      return [current.deleteFromDecimal(), this];
+    }
   }
 }
