fdd0aeca6a016334b03ea5c94815ec752751fbaf
Yehuda Katz <wycats@gmail.com>
2019-04-01 08:25:22 -0700

    Extract number to prep for decimal

--- app/components/calculator/component.js
+++ app/components/calculator/component.js
@@ -3,24 +3,51 @@ import { action } from "@ember/object";
 import { tracked } from "@glimmer/tracking";
 
 export default class Calculator extends Component {
-  @tracked current = 0;
+  @tracked current = new CalculatorNumber(0, 0);
+
+  get rendered() {
+    return this.current.rendered();
+  }
 
   @action
   append(e) {
-    this.current = this.current * 10 + parseInt(e.target.value);
+    this.current = this.current.appendToWhole(parseInt(e.target.value));
   }
 
   @action
   zero() {
-    if (this.current === 0) {
-      return;
-    } else {
-      this.current = this.current * 10;
-    }
+    this.current = this.current.appendZero();
   }
 
   @action
   backspace() {
-    this.current = Math.floor(this.current / 10)
+    this.current = this.current.deleteFromWhole();
+  }
+}
+
+class CalculatorNumber {
+  constructor(whole, decimal) {
+    this.whole = whole;
+    this.decimal = decimal;
+  }
+
+  rendered() {
+    if (this.decimal === 0) {
+      return `${this.whole}`;
+    } else {
+      return `${this.whole}.${this.decimal}`;
+    }
+  }
+
+  appendToWhole(number) {
+    return new CalculatorNumber(this.whole * 10 + number, this.decimal);
+  }
+
+  appendZero() {
+    return new CalculatorNumber(this.whole * 10, this.decimal);
+  }
+
+  deleteFromWhole() {
+    return new CalculatorNumber(Math.floor(this.whole / 10), this.decimal);
   }
 }
