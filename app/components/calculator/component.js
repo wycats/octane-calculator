14b424e442a093cdb89b71c00cf741885e2585d6
Yehuda Katz <wycats@gmail.com>
2019-04-01 08:08:02 -0700

    Handle numbers

---
+++ app/components/calculator/component.js
@@ -0,0 +1,22 @@
+import Component from "@glimmer/component";
+import { action } from "@ember/object";
+import { tracked } from "@glimmer/tracking";
+
+export default class Calculator extends Component {
+  @tracked current = 0;
+
+  @action
+  append(e) {
+    this.current = this.current * 10 + parseInt(e.target.value);
+  }
+
+  @action
+  zero() {
+    if (this.current === 0) {
+      return;
+    } else {
+      this.current = this.current * 10;
+    }
+  }
+
+}
